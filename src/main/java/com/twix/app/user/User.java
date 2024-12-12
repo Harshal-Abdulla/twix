f92246d431ecab410ce2b7db792a5d07bf0aa376
Rahul Gopinathan <rahulthettikulam@gmail.com>
2024-12-12 10:19:15 +0000

    The Jackson library, which Spring uses for JSON serialization, requires proper getters for all fields that need to be serialized. Public getters are required for this. Added getters for user class

--- src/main/java/com/twix/app/user/User.java
+++ src/main/java/com/twix/app/user/User.java
@@ -14,4 +14,17 @@ public class User {
 
     @Column(nullable = false)
     private String name;
+
+    // Getters
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
