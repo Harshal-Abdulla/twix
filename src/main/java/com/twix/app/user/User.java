94d0f1b7ad06ce444ccbe6286810309ae972147e
lee <mcli2023@outlook.com>
2024-12-13 16:35:19 +0000

    incorporated observer pattern into the existing code

--- src/main/java/com/twix/app/user/User.java
+++ src/main/java/com/twix/app/user/User.java
@@ -10,8 +10,15 @@ public class User implements Observer {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
+    private String name;
+
     @Column(unique = true, nullable = false)
     private String username;
+
+    public String getName() {
+        return name;
+    }
 
     public String getUsername() {
         return username;
